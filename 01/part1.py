cdb9d68af303b9c0ecd7b117bd0f304407ff4e2d
Darren Richardson <mnestis@users.noreply.github.com>
2018-07-05 11:43:30 +0100

    Day 1, Part 1

---
+++ 01/part1.py
@@ -0,0 +1,17 @@
+
+def read_input(input_location):
+    with open(input_location, "r") as input_file:
+        input_data = input_file.read().strip()
+    return input_data
+
+def calculate_captcha(input_data):
+    pairs = list(zip(input_data[:-1], input_data[1:])) + [(input_data[0], input_data[-1])]
+
+    print(pairs)
+    return sum([(int(pair[0]) if pair[0] == pair[1] else 0) for pair in pairs])
+
+
+if __name__ == "__main__":
+    INPUT_LOCATION = "input.txt"
+    INPUT_DATA = read_input(INPUT_LOCATION)
+    print(calculate_captcha(INPUT_DATA))
