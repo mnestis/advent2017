6c261e81665da295775623a7fc0e747b559849d8
Darren Richardson <mnestis@users.noreply.github.com>
2018-07-05 16:05:31 +0100

    Day 6, Part 2

---
+++ 06/part2.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def read_input(filename):
+    with open(filename, "r") as input_file:
+        return tuple([int(i) for i in input_file.read().strip().split("\t")])
+
+def balance_stacks(starting_configuration):
+
+    counter = 0
+    current_stacks = list(starting_configuration)
+    previous_stacks = set()
+    previous_stacks.add(starting_configuration)
+    in_loop = False
+
+    while True:
+        counter += 1
+        bin_index = np.argmax(current_stacks)
+        hopper = current_stacks[bin_index]
+        current_stacks[bin_index] = 0
+
+        while hopper:
+            bin_index += 1
+            if bin_index == len(current_stacks):
+                bin_index = 0
+            current_stacks[bin_index] += 1
+            hopper -= 1
+        
+        if tuple(current_stacks) in previous_stacks:
+            if in_loop:
+                return len(previous_stacks)
+            else:
+                in_loop = True
+                previous_stacks = set()
+        previous_stacks.add(tuple(current_stacks))
+
+if __name__ == "__main__":
+    INPUT = read_input("06/input.txt")
+    print(balance_stacks(INPUT))
