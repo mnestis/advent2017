4d26b63bd0d7060946ef0578ccf26a476cb06027
Darren Richardson <mnestis@users.noreply.github.com>
2018-07-05 14:56:38 +0100

    Day 4, Part 2

---
+++ 04/part2.py
@@ -0,0 +1,21 @@
+
+
+def read_input(input_filename):
+    with open(input_filename, "r") as input_file:
+        return [line.strip().split(" ") for line in input_file.readlines()]
+
+def validate_passphrase(phrase):
+    already_parsed = set()
+    for word in phrase:
+        word = tuple(sorted(list(word)))
+        if word in already_parsed:
+            return False
+        already_parsed.add(word)
+    return True
+
+if __name__ == "__main__":
+    INPUT = read_input("input.txt")
+    valid_count = 0
+    for phrase in INPUT:
+        valid_count += 1 if validate_passphrase(phrase) else 0
+    print(valid_count)
