dddffa40634072c827d61d120b8b83357b426c31
Darren Richardson <noreply@mnestis.net>
2018-09-18 20:23:45 +0100

    Day 12, Part 1

---
+++ 12/part1.py
@@ -0,0 +1,39 @@
+
+from pathlib import Path
+
+def group_membership(input_str):
+    rows = input_str.split("\n")
+
+    direct_connections = {}
+    for row in rows:
+        program_id, conns = row.split(" <-> ")
+        direct_connections[program_id] = {}
+        direct_connections[program_id] = set(conns.split(", "))
+        direct_connections[program_id].add(program_id)
+
+    print(direct_connections)
+
+    current_reach = 0
+    reachable = direct_connections["0"]
+    while True:
+        for program_id in list(reachable):
+            reachable.update(direct_connections[program_id])
+
+        if len(reachable) == current_reach:
+            break
+        else:
+            current_reach = len(reachable)
+    
+    return current_reach
+    
+
+
+
+if __name__ == "__main__":
+    INPUT_PATH = Path.cwd() / "input.txt"
+    assert INPUT_PATH.exists()
+
+    with open(INPUT_PATH, "r") as input_file:
+        input_str = input_file.read().strip()
+
+    print(group_membership(input_str))    
